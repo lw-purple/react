240c9df20e178cf025f243796b03ef3cc0667f95
wangzailong <1715587588@qq.com>
2017-05-05 17:35:06 +0800

    react radux

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyCxDhp8BN5TOJDfJRF1RudsccJA7zFEf_k'
+
+
+
+//  Create a new component. This component should produce
+//  some HTML
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+             videos: [],
+             selectedVideo:null 
+            };
+        this.videoSearch('surfboards');
+    }
+    videoSearch(term){
+        YTSearch({ key: API_KEY, term: term }, videos => {
+            this.setState({
+                videos,
+                selectedVideo:videos[0]
+            });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList onVideoSelect={video => this.setState({selectedVideo:video})} videos={this.state.videos}/>
+            </div>
+        );
+
+    }
+}
+//  Take this component's generated HTML and put it 
+//  on the page ( in the DOM )
+
+ReactDOM.render(<App />, document.querySelector('.container'));
